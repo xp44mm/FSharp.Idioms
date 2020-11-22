44635d3589495482fe8d5c44a9801ae762565ebc
xp44mm <34696643@qq.com>
2020-11-22 18:58:10 +0800

    keyset

--- FSharp.Idioms/Map.fs
+++ FSharp.Idioms/Map.fs
@@ -3,7 +3,7 @@ module FSharp.Idioms.Map
 
 open System.Collections.Generic
 
-/// 收集可能重复的键，值变成值的序列
+/// 收集可能重复的键，分组的值集合类型信息丢弃，返回seq类型。如输入(string*obj)[]类型，输出将是Map<string,seq<obj>>类型。
 let fromDuplicateKeys (pairs:#seq<'k*'v>) =
     pairs
     |> Seq.groupBy fst
@@ -14,6 +14,8 @@ let fromDuplicateKeys (pairs:#seq<'k*'v>) =
     )
     |> Map.ofSeq
 
+/// 值不变，修改键。
+/// 返回的键有重复，值将合并进seq<'v>中。合并后的值集将至少有一个元素。
 let mapKey (getKey:'k->'v->'kk) (mp:Map<'k,'v>) =
     mp
     |> Map.toSeq
@@ -22,12 +24,16 @@ let mapKey (getKey:'k->'v->'kk) (mp:Map<'k,'v>) =
         kk,v)
     |> fromDuplicateKeys
 
+/// 整个词条包括键，包括值，都需要改变。
+/// 返回的键有重复，值将合并进seq<'v>中。合并后的值集将至少有一个元素。
 let mapEntry (getEntry:'k->'v->'kk*'vv) (mp:Map<'k,'v>) =
     mp
     |> Map.toSeq
     |> Seq.map(fun(k,v)-> getEntry k v)
     |> fromDuplicateKeys
 
+/// 同Seq.concat
+/// 返回的键有重复，值将合并进seq<'v>中。合并后的值集将至少有一个元素。
 let concat (mps:#seq<Map<'k,'v>>) =
     mps
     |> Seq.collect Map.toSeq
@@ -36,6 +42,7 @@ let concat (mps:#seq<Map<'k,'v>>) =
 let append (mp1:Map<'k,'v>) (mp2:Map<'k,'v>) =
     concat [mp1;mp2]
 
+/// 获取键的集合(Set)
 let keys (mp:Map<'k,'v>) =
     let i = mp :> IDictionary<'k,'v>
-    i.Keys :> seq<'k>
+    i.Keys |> Set.ofSeq
