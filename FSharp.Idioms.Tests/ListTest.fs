a76f3b34ca80d19eb7ab213d8c7e22a4e1de80eb
xp44mm <34696643@qq.com>
2021-01-05 09:52:07 +0800

    copy methods to list from array

--- FSharp.Idioms.Tests/ListTest.fs
+++ FSharp.Idioms.Tests/ListTest.fs
@@ -11,14 +11,89 @@ type ListTest(output : ITestOutputHelper) =
         res 
         |> Render.stringify
         |> output.WriteLine
+    [<Fact>]
+    member this.``Pair List toMap``() =
+        // 有重复键
+        let pairs = [
+            1,1
+            1,2
+            1,2
+            2,1
+            2,1
+            2,2
+        ]
+
+        let y = List.toMap pairs
+        //show y
+        let res = 
+            Map.ofList [
+                1,[1;2;2];
+                2,[1;1;2];
+                ]
+        Should.equal y res
 
     [<Fact>]
-    member this.``test penetrate``() =
+    member this.``Triple List toJaggedMap``() =
+        // 有重复键
+        let triples = 
+            [
+                (1,1,1)
+                (1,2,2)
+                (1,2,3)
+                (2,1,4)
+                (2,1,5)
+                (2,2,6)
+            ]
+        let y = List.toJaggedMap triples
+        //show y
+        let res = 
+            Map.ofList [
+                1,Map.ofList [
+                    1,[1];
+                    2,[2;3]
+                    ];
+                2,Map.ofList [
+                    1,[4;5];
+                    2,[6]
+                    ];
+                ]
+        Should.equal y res
+
+    [<Fact>]
+    member this.``ofJaggedMap to Triple List``() =
+        let jaggedMap =
+            Map.ofList [
+                1,Map.ofList [
+                    1,1;
+                    2,2;
+                    ];
+                2,Map.ofList [
+                    1,3;
+                    2,4;
+                    ];
+                ]
+
+        let y = List.ofJaggedMap jaggedMap
+
+        //show y
+        let res = 
+            [
+                (1,1,1)
+                (1,2,2)
+                (2,1,3)
+                (2,2,4)
+            ]
+
+        Should.equal y res
+
+
+    [<Fact>]
+    member this.``test takeUntilNot``() =
         let ls = [1;2;3;4;5]
         
         let y = 
             ls
-            |> List.penetrate (fun i -> i<=3) 
+            |> List.takeUntilNot (fun i -> i<=3)
 
         let res = [1;2;3;4]
         Should.equal y res
--- FSharp.Idioms/List.fs
+++ FSharp.Idioms/List.fs
@@ -1,8 +1,54 @@
 ﻿[<RequireQualifiedAccess>]
 module FSharp.Idioms.List
 
+/// 将键值对列表转化为Map，键可能重复的，相同键的值收集到列表中。
+let toMap (pairs:('k*'v)list) =
+    pairs
+    |> List.groupBy fst
+    |> List.map(fun(k,pairs)->
+        let values =
+            pairs |> List.map snd
+        k,values
+    )
+    |> Map.ofList
+
+/// 将键值对列表转化为二级JaggedMap，键可能重复的，相同键的值收集到列表中。
+let toJaggedMap (triples:('x*'y*'z) list) =
+    triples
+    |> List.groupBy Triple.first
+    |> List.map(fun(x,triples)->
+        let mp =
+            triples
+            |> List.map Triple.lastTwo
+            |> toMap
+        x,mp)
+    |> Map.ofList
+
+/// 将键值对列表转化为二级JaggedMap，相同键的值后来者赢。
+let toUniqueJaggedMap (triples:('x*'y*'z)list) =
+    triples
+    |> List.groupBy Triple.first
+    |> List.map(fun(x,triples)->
+        let mp =
+            triples
+            |> List.map Triple.lastTwo
+            |> Map.ofList
+        x,mp)
+    |> Map.ofList
+
+/// 从二级JaggedMap构造列表，生成三元组的列表
+let ofJaggedMap (mp:Map<'u,Map<'v,'w>>) =
+    mp
+    |> Map.toList
+    |> List.map(fun(u, v) ->
+        v
+        |> Map.toList
+        |> List.map(fun(v,w)->u,v,w)
+    )
+    |> List.concat
+
 /// 取列表前面所有断言为真的项，加上第一个断言为假的项（如果有）
-let penetrate (predicate:'t->bool) (ls:'t list) =
+let takeUntilNot (predicate:'t->bool) (ls:'t list) =
     let rec loop (acc:'t list) (ls:'t list) =
         match ls with
         | [] -> acc
