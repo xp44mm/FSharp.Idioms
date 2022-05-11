3ec8dd00928b01bb5206d81b4eb9f0fd549bce5f
xp44mm <34696643@qq.com>
2022-05-11 17:40:01 +0800

    1.2.6

---
+++ FSharp.Idioms.Tests/RegularExpressionsTest.fs
@@ -0,0 +1,22 @@
+﻿namespace FSharp.Idioms
+open FSharp.Idioms.RegularExpressions
+open Xunit
+open Xunit.Abstractions
+open System
+open FSharp.xUnit
+open System.Text.RegularExpressions
+
+type RegularExpressionsTest(output: ITestOutputHelper) =
+
+    [<Fact>]
+    member this.``Active Pattern Search``() =
+        let x = "123xyz"
+        match x with
+        | Search(Regex(@"^\d+")) ma ->
+            //https://docs.microsoft.com/en-us/dotnet/api/system.text.regularexpressions.match?view=net-6.0
+            let raw = ma.Value
+            let rest = x.[ma.Length..]
+            Should.equal "123" raw
+            Should.equal "xyz" rest
+        | _ -> failwith $"{x}"
+
---
+++ FSharp.Idioms/RegularExpressions.fs
@@ -0,0 +1,14 @@
+﻿module FSharp.Idioms.RegularExpressions
+open System
+open System.Text.RegularExpressions
+
+/// represents the first pattern match in a string.
+let trySearch (re: Regex) (input:string) =
+    let m = re.Match(input)
+    if m.Success then
+        Some(m)
+    else
+        None
+
+/// represents the first pattern match in a string.
+let (|Search|_|) (re: Regex) = trySearch re
